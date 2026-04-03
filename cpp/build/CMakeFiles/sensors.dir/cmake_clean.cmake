94f69fd6f3e8ee77ade36e1511275d46d9715ac5
greetrift <danyashox@mail.ru>
2026-04-04 01:46:18 +0300

    либа с функциями возвращающими фикс значения

---
+++ cpp/build/CMakeFiles/sensors.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sensors.dir/sensors.cpp.o"
+  "CMakeFiles/sensors.dir/sensors.cpp.o.d"
+  "libsensors.pdb"
+  "libsensors.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sensors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/build/CMakeFiles/sensors.dir/sensors.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/sensors.dir/sensors.cpp.o: \
+ /home/greetrift/cputracker/cpp/sensors.cpp /usr/include/stdc-predef.h
