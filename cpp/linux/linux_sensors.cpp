6fd30d6e0ddee863406f4edbaa15ff31c6af0243
greetrift <danyashox@mail.ru>
2026-04-17 00:06:53 +0300

    linux methods done

--- cpp/linux/linux_sensors.cpp
+++ cpp/linux/linux_sensors.cpp
@@ -32,3 +32,21 @@ double get_battery_percent() {
   file2 >> value2;
   return std::max(value1, value2);
 }
+
+int get_fan_speed_rpm() {
+  std::string path = "/sys/class/hwmon/hwmon*/fan*_input";
+  glob_t glob_result;
+  glob("/sys/class/hwmon/hwmon*/fan*_input", GLOB_TILDE, NULL, &glob_result);
+  int mx_fan_speed = 0;
+  for (int i = 0; i < glob_result.gl_pathc; i++) {
+    std::ifstream file(glob_result.gl_pathv[i]);
+    int fan_speed = 0;
+    if (file >> fan_speed) {
+      if (fan_speed > mx_fan_speed) {
+        mx_fan_speed = fan_speed;
+      }
+    }
+  }
+  globfree(&glob_result);
+  return mx_fan_speed;
+}
